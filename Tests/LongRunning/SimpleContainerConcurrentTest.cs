9f5d6c2ab5f8826ed7620dde48fb847f7c7b65fb
gusev_p <gusev_p@skbkontur.ru>
2014-12-02 21:49:06 +0500

    тест на тупизну, если свалились в другом потоке

--- Tests/LongRunning/SimpleContainerConcurrentTest.cs
+++ Tests/LongRunning/SimpleContainerConcurrentTest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading;
 using NUnit.Framework;
 
-namespace SimpleContainer.Tests
+namespace SimpleContainer.Tests.LongRunning
 {
 	public abstract class SimpleContainerConcurrentTest: SimpleContainerTestBase
 	{
