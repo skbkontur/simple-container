43f9c25347ffa7e6f78ad50cf0d8c0cee67dd21c
gusev_p <gusev_p@skbkontur.ru>
2015-02-12 16:02:07 +0500

    temp

---
+++ _Src/Tests/GenericsHandlingTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using SimpleContainer.Configuration;
+using SimpleContainer.Tests.Helpers;
+
+namespace SimpleContainer.Tests
+{
+	public abstract class GenericsHandlingTest : SimpleContainerTestBase
+	{
+		public class Bug : GenericsHandlingTest
+		{
+			public class A<T>
+			{
+				public readonly int value;
+
+				public A(int value)
+				{
+					this.value = value;
+				}
+			}
+
+			public class B
+			{
+				public readonly A<int> a;
+
+				public B(A<int> a)
+				{
+					this.a = a;
+				}
+			}
+
+			public class AConfigurator : IServiceConfigurator<A<int>>
+			{
+				public void Configure(ConfigurationContext context, ServiceConfigurationBuilder<A<int>> builder)
+				{
+					builder.Dependencies(new {value = 42});
+				}
+			}
+
+			[Test]
+			public void Test()
+			{
+				var container = Container();
+				Assert.That(container.Get<B>().a.value, Is.EqualTo(42));
+			}
+		}
+	}
+}
