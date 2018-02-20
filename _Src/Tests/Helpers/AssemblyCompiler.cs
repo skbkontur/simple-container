7a7e12e95428513c614618dc9eae12c501a71697
halex2005 <halex2005@e1.ru>
2018-02-21 03:27:59 +0500

    use roslyn compilation in tests

--- _Src/Tests/Helpers/AssemblyCompiler.cs
+++ _Src/Tests/Helpers/AssemblyCompiler.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.CodeDom.Compiler;
 using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using NUnit.Framework;
 using SimpleContainer.Interface;
 using SimpleContainer.Helpers;
@@ -32,25 +33,35 @@ namespace SimpleContainer.Tests.Helpers
 
 		public static Assembly Compile(string source, string resultFileName, params Assembly[] references)
 		{
-			var compilationParameters = new CompilerParameters
+			var syntaxTree = CSharpSyntaxTree.ParseText(source);
+			var assemblyName = "tmp_" + Guid.NewGuid().ToString("N");
+			var assemblyPath = resultFileName ?? assemblyName + ".dll";
+
+			var metadataReferences = references
+				.Select(r => MetadataReference.CreateFromFile(r.Location))
+				.Concat(MetadataReference.CreateFromFile(typeof(object).Assembly.Location));
+			var defaultNamespaces = new[] { "System" };
+			var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+				.WithUsings(defaultNamespaces);
+			
+			var compilation = CSharpCompilation.Create(
+				assemblyName,
+				new[] { syntaxTree },
+				metadataReferences,
+				compilationOptions);
+			
+			using (var dllStream = File.OpenWrite(assemblyPath))
 			{
-				OutputAssembly = resultFileName ?? "tmp_" + Guid.NewGuid().ToString("N") + ".dll",
-				GenerateExecutable = false
-			};
-			foreach (var reference in references.Concat(defaultAssemblies).Select(x => x.Location))
-				compilationParameters.ReferencedAssemblies.Add(reference);
-			CompilerResults compilationResult;
-			using (var codeDomProvider = CodeDomProvider.CreateProvider("C#"))
-				compilationResult = codeDomProvider.CompileAssemblyFromSource(compilationParameters, source);
-			if (compilationResult.Errors.HasErrors || compilationResult.Errors.HasWarnings)
-			{
-				var message = compilationResult.Errors
-					.Cast<CompilerError>()
-					.Select(x => string.Format("{0}:{1} {2}", x.Line, x.Column, x.ErrorText))
-					.JoinStrings("\r\n");
-				Assert.Fail(message);
+				var emitResult = compilation.Emit(dllStream);
+				if (!emitResult.Success)
+				{
+					var message = emitResult.Diagnostics
+						.Select(d => $"{d.Location}: {d.Severity} {d.Id}: {d.GetMessage()}")
+						.JoinStrings("\r\n");
+					Assert.Fail(message);
+				}
 			}
-			return compilationResult.CompiledAssembly;
+			return Assembly.LoadFrom(assemblyPath);
 		}
 
 		private static void CleanupTestAssemblies()
