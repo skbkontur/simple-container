68386076e23e2b208444c43e2dd7acedd48c12bf
akharlov <harl_aa@skbkontur.ru>
2020-06-01 15:40:24 +0500

    fix .net core installation for running tests in travis and appveyor

--- nuget/publish.cmd
+++ nuget/publish.cmd
@@ -2,5 +2,5 @@
 set nugetPath=C:\tools\nuget.exe
 del *.nupkg
 call ..\build.cmd || exit /b -1
-call %nugetPath% pack simple-container.nuspec || exit /b -1
+call %nugetPath% pack simple-container.nuspec -Properties Configuration=Release;Version=1.0.6 || exit /b -1
 %nugetPath% push *.nupkg -Source https://www.nuget.org/api/v2/package
