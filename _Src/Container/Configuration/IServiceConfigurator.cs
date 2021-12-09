f292a21437f952b642347d6b54b50d1208fc9eed
iperevoschikov <iperevoschikov@skbkontur.ru>
2021-12-09 10:17:54 +0500

    Mark inheritors of `IServiceConfigurator` as implicitly used

--- _Src/Container/Configuration/IServiceConfigurator.cs
+++ _Src/Container/Configuration/IServiceConfigurator.cs
@@ -1,5 +1,8 @@
+using JetBrains.Annotations;
+
 namespace SimpleContainer.Configuration
 {
+	[UsedImplicitly(ImplicitUseTargetFlags.WithInheritors)]
 	public interface IServiceConfigurator<TService>
 	{
 		void Configure(ConfigurationContext context, ServiceConfigurationBuilder<TService> builder);
